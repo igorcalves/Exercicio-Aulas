e05749f5423cf6f662b71da1ce7176a595b81579
igor alves <igor193@live.com>
2022-09-17 05:07:11 +0000

    17/09

---
+++ Lambdas/CalculoTeste2.java
@@ -0,0 +1,18 @@
+package Lambdas;
+
+class CalculoTeste2 {
+    public static void main(String[] args) {
+      
+      Calculo soma = (x, y)->{return x + y ;};
+      
+      System.out.println(soma.executar(2,3));
+      
+      soma = (x,y) -> x * y;
+      
+      System.out.println(soma.executar(2,3 ));
+        System.out.println(soma.legal());
+      System.out.println(Calculo.muitoLegal());
+    
+    }
+    
+  }
